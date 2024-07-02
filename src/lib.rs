ad6c25cfff340d2441b27ebaad27d1270e57d44d
Andy Gayton <andy@thecablelounge.com>
2024-07-02 12:18:16 -0400

    chore: update for nushell 0.95

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,10 @@ pub use commands::SSE;
 pub use plugin::FromPlugin;
 
 impl Plugin for FromPlugin {
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
+
     fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![Box::new(SSE)]
     }
